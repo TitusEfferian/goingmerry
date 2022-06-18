462ed29059f8d84700e6895c177c9de66703caab
Titus Efferian <kiritosato@gmail.com>
2022-06-18 04:06:57 +0000

    test swr

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import { AppShell, MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Helmet } from "react-helmet";
 import { lazy, useEffect, useState } from "react";
+import { SWRConfig } from "swr";
 
 const Home = lazy(() => import(/* webpackChunkName: "Home" */ "./routes/Home"));
 
 function App() {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: "dark" }}
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
     >
-      <ModalsProvider>
-        <AppShell>
-          <Helmet>
-            <title>Going Merry Clock</title>
-          </Helmet>
-          <Home />
-        </AppShell>
-      </ModalsProvider>
-    </MantineProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "dark" }}
+      >
+        <ModalsProvider>
+          <AppShell>
+            <Helmet>
+              <title>Going Merry Clock</title>
+            </Helmet>
+            <Home />
+          </AppShell>
+        </ModalsProvider>
+      </MantineProvider>
+    </SWRConfig>
   );
 }
 
--- src/routes/Home/components/CustomClockList.tsx
+++ src/routes/Home/components/CustomClockList.tsx
@@ -1,7 +1,10 @@
 import { Card, Group } from "@mantine/core";
+import useSWR from "swr";
 import CustomClockCard from "./CustomClockCard";
 
 const CustomClockList = () => {
+  const {data} = useSWR('http://worldtimeapi.org/api/timezone')
+  console.log(data)
   return (
     <Group>
       <CustomClockCard />
